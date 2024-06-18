b617c141ef2aa22118f1b8009f7f55f2ed767bf6
Taher <taherhossieninasab@gmail.com>
2024-06-18 17:08:12 +0330

    second commit

--- src/pages/ProductPageMoreInfo.js
+++ src/pages/ProductPageMoreInfo.js
@@ -2,7 +2,7 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {Link, useParams} from 'react-router-dom';
 import {ProductContext} from "../contexts/ProductContext";
-
+import './ProductPageMoreInfo.css'
 const ProductPageMoreInfo = () => {
     const { productList } = useContext(ProductContext);
     const { id } = useParams();
@@ -18,21 +18,21 @@ const ProductPageMoreInfo = () => {
     }
 
     return (
-        <div className='content-item'>
+        <div className='ProductPage-content-item'>
             <Link to={`/product/${product.id}`}>
-                <img className='content-img' src={product.images[0]} alt={product.name}/>
+                <img className='ProductPage-content-img' src={product.images[0]} alt={product.name}/>
             </Link>
-            <div className='content-main'>
-                <p className='content-main-title'>{product.name}</p>
-                <p className='content-main-description'>
-                    {(product.description, 50)}{' '}
-                    <Link to={`/product/${product.id}`} className="read-more">ادامه مطلب</Link>
+            <div className='ProductPage-content-main'>
+                <p className='ProductPage-content-main-title'>{product.name}</p>
+                <p className='ProductPage-content-main-description'>
+                    {product.description}
+                    {/*<Link to={`/product/${product.id}`} className="read-more">ادامه مطلب</Link>*/}
                 </p>
             </div>
-            <div className='flex-md-row flex-lg-column-reverse flex-xl-row content-footer'>
+            <div className='flex-md-row flex-lg-column-reverse flex-xl-row ProductPage-content-footer'>
                 <button type="submit" className='btn-grad' >خرید</button>
-                <div className='content-footer-price'>
-                    <span className='content-footer-price-caption'>قیمت</span>
+                <div className='ProductPage-content-footer-price'>
+                    <span className='ProductPage-content-footer-price-caption'>قیمت</span>
                     <span><span className='me-1'>{product.price.toLocaleString('fa')}</span>تومان</span>
                 </div>
             </div>
